9ea1941b2b0550a7c0041c3af81da2a72238b5d3
Faiz <faizkhan8225@gmail.com>
2024-02-24 13:25:37 +0530

    Add folders and auth routes structure

---
+++ controllers/auth.controller.js
@@ -0,0 +1,11 @@
+exports.getSignup = (req, res, next) => {
+  res.render("signup");
+};
+
+exports.postSignup = (req, res, next) => {};
+
+exports.getLogin = (req, res, next) => {
+  res.render("login");
+};
+
+exports.postLogin = (req, res, next) => {};
